eb6465d0bbee9d86e109bcfc7bb071cbfbfa2533
Akhilesh Yadav <ydvakhilesh16@gmail.com>
2025-03-08 11:51:28 +0530

    add createExpense controller

---
+++ controller/createExpense.js
@@ -0,0 +1,41 @@
+const Finance = require("../models/finance");
+const User = require("../models/user");
+
+exports.createExpense = async(req,res)=>{
+    try{
+        const {title,category,amount,date} = req.body;
+        if(!title || !category || !amount){
+            return res.status(500).json({
+                success:false,
+                message:"all field required"
+            })
+        }
+        
+        const {id} = req.existingUser;
+
+        const response = await Finance.create({title,category,amount,date,user:id});
+
+        await User.findByIdAndUpdate(id,
+            {
+                $push: {
+                    expenses : response._id,
+                },
+            },
+            {new:true,}
+        );
+
+
+        res.status(200).json({
+            success:true,
+            data:response,
+            message:'Entry of Expense created successfully',
+        })
+    }
+    catch(err){
+        console.error("error is in create expense : ",err.message);
+        res.status(500).json({
+            success:false,
+            message:err.message,
+        })
+    }
+}
