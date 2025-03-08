c466de58b553dbf312b4575adc27f2aa09075a11
Akhilesh Yadav <ydvakhilesh16@gmail.com>
2025-03-08 11:37:50 +0530

    add models

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT || 4001;
+const dbConnect = require("./config/database");
 
 app.use(express.json());
 
-app.use(cors("*"))
+
+app.listen(PORT,()=>{
+    console.log("Server is started at :"+PORT)
+})
+
+dbConnect();
